446b2e86c993766b145bc2fb5de1dc4fd94de2e1
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-22 12:15:57 +0200

    update the cobratoolbox

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -2,7 +2,18 @@
 
 # launch MATLAB
 if [ "$ARCH" == "Linux" ]; then
+
+    export CURRENTDIR=`pwd`
+
+    # update the COBRA Toolbox
+    cd $ARTENOLIS_DATA_PATH/scratch/cobratoolbox
+    git pull origin master
+
+    cd CURRENTDIR
+
+    # launch the test suite
     $ARTENOLIS_SOFT_PATH/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash < test/testAll.m
+
 fi
 
 CODE=$?
