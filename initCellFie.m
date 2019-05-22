eeadf3812a973ed3ba625ca90cc1a5c0b4554bf7
laurentheirendt <laurent.heirendt@uni.lu>
2019-05-22 12:16:15 +0200

    add initialization of cobratoolbox

--- initCellFie.m
+++ initCellFie.m
@@ -1,2 +1,16 @@
+% set the cellfie directory
 CELLFIEDIR = fileparts(mfilename('fullpath'));
+
+% initialize the COBRA Toolbox
+if isdefined('CBTDIR')
+    cd(CBTDIR);
+    initCobraToolbox
+else
+    error('Please initialize the COBRA Toolbox or set the main directory of the COBRA Toolbox CBTDIR');
+end
+
+% change to the cellfie main directory
+cd(CELLFIEDIR);
+
+% add the entire cellfie repository to the MATLAB path
 addpath(genpath(CELLFIEDIR));
