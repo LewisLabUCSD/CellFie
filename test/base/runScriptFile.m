b10863df16491491be62cb30fd673f8adad24845
Loic Marx <loic.marx@gmail.com>
2019-05-27 10:21:49 +0200

    change COBRA variable  to CELLFIE

--- test/base/runScriptFile.m
+++ test/base/runScriptFile.m
@@ -20,20 +20,20 @@ function result = runScriptFile(fileName)
 
 global CBT_MISSING_REQUIREMENTS_ERROR_ID
 
-COBRA_TESTSUITE_TESTFILE = fileName;
+CELLFIE_TESTSUITE_TESTFILE = fileName;
 
 % get the timinig (and hope these values are not overwritten.
-COBRA_TESTSUITE_STARTTIME = clock();
+CELLFIE_TESTSUITE_STARTTIME = clock();
 
 try
     % run the file
     executefile(fileName);
 catch ME
     % vatch errors and interpret them
-    clearvars -except ME COBRA_TESTSUITE_STARTTIME COBRA_TESTSUITE_TESTFILE CBT_MISSING_REQUIREMENTS_ERROR_ID
-    scriptTime = etime(clock(), COBRA_TESTSUITE_STARTTIME);
+    clearvars -except ME CELLFIE_TESTSUITE_STARTTIME CELLFIE_TESTSUITE_TESTFILE CBT_MISSING_REQUIREMENTS_ERROR_ID
+    scriptTime = etime(clock(), CELLFIE_TESTSUITE_STARTTIME);
     result = struct('status', 'failed', 'failed', true, 'passed', false, 'skipped', false, 'fileName', ...
-                    COBRA_TESTSUITE_TESTFILE, 'time', scriptTime, 'statusMessage', 'fail', 'Error', ME);
+                    CELLFIE_TESTSUITE_TESTFILE, 'time', scriptTime, 'statusMessage', 'fail', 'Error', ME);
     if strcmp(ME.identifier, CBT_MISSING_REQUIREMENTS_ERROR_ID)
         % requirement missing, so the test was skipped.
         result.status = 'skipped';
@@ -50,10 +50,10 @@ catch ME
 end
 
 % get the timinig.
-scriptTime = etime(clock(), COBRA_TESTSUITE_STARTTIME);
+scriptTime = etime(clock(), CELLFIE_TESTSUITE_STARTTIME);
 
 result = struct('status', 'passed', 'failed', false, 'passed', true, 'skipped', false, 'fileName', ...
-                COBRA_TESTSUITE_TESTFILE, 'time', scriptTime, 'statusMessage', 'success', 'Error', MException('', ''));
+                CELLFIE_TESTSUITE_TESTFILE, 'time', scriptTime, 'statusMessage', 'success', 'Error', MException('', ''));
 
 end
 
