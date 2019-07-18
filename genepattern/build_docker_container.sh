1c8da50882392c1c41c274292696f63b4967d245
Alex Wenzel <atwenzel@eng.ucsd.edu>
2019-07-18 14:58:04 -0700

    working dockerfile checkpoint - needs slight modification for gp

---
+++ genepattern/build_docker_container.sh
@@ -0,0 +1,1 @@
+docker build -f genepattern/Dockerfile -t cellfie .
---
+++ genepattern/run_cellfie_in_docker.sh
@@ -0,0 +1,1 @@
+ docker run --rm -v $PWD:/IO atwenzel/cellfie:1.3 /usr/local/MATLAB/MATLAB_Runtime/v94 test/suite/dataTest.mat 3 MT_recon_2_2_entrez.mat local value minmaxmean 25 75
